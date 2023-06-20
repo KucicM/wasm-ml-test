180566d7e1d207f27aafd32bc8698d7d789711ab
kucicm <10778553+KucicM@users.noreply.github.com>
2023-06-21 00:27:04 +0200

    tset

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 import { InferenceSession, Tensor } from "onnxruntime-web";
 
-async function runInference() {
-  const text = document.getElementById('textbox').value;
-  const modelPath = 'model.onnx';
-  let session = new InferenceSession();
-  await session.loadModel(modelPath);
+document.addEventListener("DOMContentLoaded", () => {
+    async function runInference() {
+    const text = document.getElementById('textbox').value;
+    const modelPath = 'model.onnx';
+    let session = new InferenceSession();
+    await session.loadModel(modelPath);
 
-  const inputTensor = new Tensor([text], 'string', [1]);
+    const inputTensor = new Tensor([text], 'string', [1]);
 
-  const outputMap = await session.run([inputTensor]);
-  const outputTensor = outputMap.values().next().value;
-  const outputData = outputTensor.data;
+    const outputMap = await session.run([inputTensor]);
+    const outputTensor = outputMap.values().next().value;
+    const outputData = outputTensor.data;
 
-  console.log('Output:', outputData);
-}
+    console.log('Output:', outputData);
+    }
+});
