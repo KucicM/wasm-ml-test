7339923f346cdac055caf7c18d0e231bf7a4315f
kucicm <10778553+KucicM@users.noreply.github.com>
2023-06-21 11:51:38 +0200

    test new model

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import StringTensorType
 
 corpus = [
-'This is the first document.',
-'This document is the second document.',
-'And this is the third one.',
-'Is this the first document?',
+'one',
+'two',
+'one',
+'two'
 ]
 
 pipeline = Pipeline([
@@ -24,3 +24,6 @@ onx = convert_sklearn(pipeline, initial_types=initial_type)
 
 with open("model.onnx", "wb") as f:
     f.write(onx.SerializeToString())
+
+
+print(pipeline.predict(corpus))
